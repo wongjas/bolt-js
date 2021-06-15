d8ef66a5fd88ac0f4187db0d7d9dcd4c93da8764
Sarah Jiang <sarahjiang@slack-corp.com>
2021-06-14 17:50:40 -0700

    try with syntax check github token

--- src/publish.js
+++ src/publish.js
@@ -8,8 +8,8 @@
 import 'dotenv/config';
 import contentful from 'contentful-management';
 
-console.log('TEST: HAS API TOKEN:', process.env.CONTENTFUL_API_TOKEN !== undefined, process.env.CONTENTFUL_API_TOKEN !== null);
+console.log('TEST: HAS API TOKEN:', process.env.CONTENTFUL_API_KEY !== undefined, process.env.CONTENTFUL_API_KEY !== null);
 const client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_API_TOKEN
+  accessToken: process.env.CONTENTFUL_API_KEY
 });
 console.log('TEST: HAS CLIENT', client);
